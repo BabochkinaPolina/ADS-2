d3ddd56b444db1a4d04f1a5a322a5e430f1f3f19
BabochkinaPolina <79091702+BabochkinaPolina@users.noreply.github.com>
2021-04-06 23:29:44 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -34,30 +34,39 @@ return pairs;
 
 int countPairs3(int *arr, int len, int value) {
   return 0;
-int pairs = 0;
+  int l = 0, r = len, middle;
+  while (l < r - 1) {
+    middle = (l + r) / 2;
+    if (arr[middle] <= value)
+      l = middle + 1;
+    else
+      r = middle;
+  }
+  len = r;
+  int pairs = 0;
   for (int a = 0; a < len - 1; a++) {
-  int l = a, r = len;
-    while (l < r-1) {
-    int middle = (l + r) / 2;
+    int l = a, r = len;
+    while (l < r - 1) {
+      middle = (l + r) / 2;
       if (arr[a] + arr[middle] == value) {
-      pairs++;
-      int b = middle+1;
-        while (arr[a] + arr[b] == value && b <r) {
         pairs++;
-        b++;
+        int b = middle + 1;
+        while (arr[a] + arr[b] == value && b < r) {
+          pairs++;
+          b++;
         }
-      b = middle - 1;
-          while (arr[a] + arr[b] == value && b > l) {
+        b = middle - 1;
+        while (arr[a] + arr[b] == value && b > l) {
           pairs++;
           b--;
-          }
-       break;
-       }
-          if (arr[a] + arr[middle] > value)
-          r = middle;
-          else
-          l = middle;
-     }
+        }
+        break;
+      }
+      if (arr[a] + arr[middle] > value)
+        r = middle;
+      else
+        l = middle;
+    }
   }
-return pairs;  
+  return pairs;
 }
